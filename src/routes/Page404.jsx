a5f8ff60a73aecf0f46285b67934412a06169338
Luvous <thomasvarela2001@outlook.es>
2023-10-29 02:08:51 -0300

    Netlify correction

--- src/routes/Page404.jsx
+++ src/routes/Page404.jsx
@@ -4,6 +4,13 @@ function Page404() {
   return (
     <section id='LostPage'>
         <h1><b>404</b><br/>YOU SEEM LOST</h1>
+        <div className='subtext'>There's no planet at that adress :/</div>
+  
+        <div className='contfly'>
+            <a href='/' className='flyhome'>
+                FLY HOME
+            </a>
+        </div>
     </section>
   )
 }
